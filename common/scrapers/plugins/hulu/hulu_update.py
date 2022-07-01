fb21f147584d907109fda903a924f082569516ba
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-07-01 05:55:54 -0700

    Hulu updating workaround

--- common/scrapers/plugins/hulu/hulu_update.py
+++ common/scrapers/plugins/hulu/hulu_update.py
@@ -22,7 +22,9 @@ class HuluUpdate(HuluBase, ScraperUpdateShared):
 
     def justwatch_update(self, justwatch_entry: dict[str, Any], date: datetime) -> None:
         # Only information on JustWatch that can be cross referenced is the title
-        show_title = justwatch_entry["show_title"]
+        # List all keys for justwatch_entry
+        # TODO: This is supposed to be a temporary workaround...
+        show_title = justwatch_entry.get("show_title") or justwatch_entry.get("title")
         show = Show.objects.filter(website=self.WEBSITE, name=show_title)
         if show:
             HuluShow(show[0]).import_all(minimum_info_timestamp=date)
